666d59e5a0a15289f378c6d447df6ef7eb63c3c5
khg8016 <92encanto@gmail.com>
2024-08-12 18:10:39 +0900

    feat: modify dist

---
+++ dist/components/bubbles/GuidedQuestionBubble.d.ts
@@ -0,0 +1,8 @@
+type Props = {
+    prompt: string;
+    onPromptClick?: () => void;
+    fontSize?: number;
+};
+export declare const GuidedQuestionBubble: (props: Props) => import("solid-js").JSX.Element;
+export {};
+//# sourceMappingURL=GuidedQuestionBubble.d.ts.map
